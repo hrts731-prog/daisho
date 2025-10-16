fb996ceae0e5374c1c26d4771bf9da432172f247
hrts731-prog <hrt_s731@icloud.com>
2025-10-16 15:18:36 +0900

    20251016v04

---
+++ daisyo-day1/script.js
@@ -0,0 +1,7 @@
+const helloBtn=document.getElementByld("helloBtn");
+const message=document.getElementByld("message");
+
+helloBtn.addEventListener("click",()=>{
+    const hour=new Date().getHours();
+    
+})
